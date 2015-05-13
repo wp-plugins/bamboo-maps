c6d70dbd0c7eb687865c17efc7260f793de0e7cf
bamboosolutions <bamboosolutions@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-13 09:06:57 +0000

    Updated to v1.3.
    git-svn-id: https://plugins.svn.wordpress.org/bamboo-maps/tags/1.3@1159500 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bamboo-maps.js
@@ -0,0 +1,41 @@
+/******************************************************************/
+
+	function bambooInitMap(el) {
+
+		var id = el.attr('id');
+		var latitude = 53.594199;
+		var longitude = -2.29522;
+		var zoom = 15;
+
+		var link = jQuery(el).html();
+		var startPos = link.indexOf('/@')+2;
+		var endPos = link.indexOf('z');
+		var query = link.substr(startPos, endPos-startPos);
+		var params = query.split(",");
+		latitude = params[0];
+		longitude = params[1];
+		zoom = parseInt(params[2]);
+
+		var options = {
+			'center': new google.maps.LatLng(latitude, longitude),
+			'zoom': zoom,
+			'mapTypeId': google.maps.MapTypeId.ROADMAP,
+			'scrollwheel':false,
+			'streetViewControl': true
+		};
+		var div = document.getElementById(id);
+		new google.maps.Map(div, options);
+
+	}
+
+/******************************************************************/
+
+	jQuery(document).ready(function(){
+
+		jQuery('.bamboo_map').each(function(){
+			bambooInitMap(jQuery(this));
+		});
+
+	});
+
+/******************************************************************/
---
+++ bamboo-maps.php
@@ -0,0 +1,44 @@
+<?php
+/**************************************************************************************************/
+/*
+Plugin Name: Bamboo Maps
+Plugin URI:  http://www.bamboosolutions.co.uk/wordpress/bamboo-maps
+Author:      Bamboo Solutions
+Author URI:  http://www.bamboosolutions.co.uk
+Version:     1.2
+Description: Bamboo Maps is the simplest way of embedding a fully functional Google Map into your content.
+*/
+/**************************************************************************************************/
+
+	function bamboo_map( $atts, $content=null ) {
+
+		do_action( 'before_bamboo_map' );
+
+		static $counter = 0;
+		$counter++;
+
+		$width = "100%";
+     	if ( isset( $atts["width"] ) ) $width = $atts["width"];
+
+		$height = "300px";
+     	if ( isset( $atts["height"] ) ) $height = $atts["height"];
+
+		$path = plugins_url( '', __FILE__ );
+
+		wp_enqueue_script( 'google-maps', 'http://maps.google.com/maps/api/js?sensor=false', array(), null, true );
+		wp_enqueue_script( 'bamboo-maps', $path.'/bamboo-maps.js', array('jquery', 'google-maps') , null, true );
+
+     	$html = "<div class=\"bamboo_map\" id=\"bamboo_map_$counter\" style=\"width:$width; height:$height;\">";
+		$html.= "<a href=\"" . strip_tags($content) ."\"></a>";
+		$html.= "</div>";
+
+		do_action( 'after_bamboo_map' );
+
+		return $html;
+
+	}
+	add_shortcode( 'bamboo-map', 'bamboo_map' );
+
+
+/**************************************************************************************************/
+?>
